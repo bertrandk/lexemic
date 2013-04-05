822ddb2768444b9867e43c54d642e588544d443c
bertrandk <b.karerangabo@gmail.com>
2013-04-05 00:01:00 -0400

    Reorganize sentiment lexicon.

--- resources/word_lists/credit.md
+++
@@ -1,6 +0,0 @@
-# Credits
-
-## http://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
-
-    positive_words.edn
-    negative_words.edn 
--- src/cljs-nlp/sentiment/simple.cljs
+++ src/cljs-nlp/sentiment/simple.cljs
@@ -1,17 +1,7 @@
 (ns cljs-nlp.sentiment.simple
   (:use [clojure.string :only [lower-case split]]
-        [cljs.reader :only [read-string]]))
-
-(def fs (js/require "fs"))
-
-;; TODO: use __dirname and __filename to get correct paths
-(def negative_words
-  (read-string (.readFileSync fs "resources/word_lists/negative_words.edn" 
-                                 "utf8")))
-
-(def positive_words 
-  (read-string (.readFileSync fs "resources/word_lists/positive_words.edn"
-                                 "utf8")))
+        [cljs.reader :only [read-string]])
+  (:require [cljs-nlp.data.sentiment :as data]))
 
 (defn collect-matches [tokens lexicon]
   (vec
@@ -26,10 +16,10 @@
   {:score hits :comparative (/ hits (count tokens)) :words words})
 
 (defn negative-rating [phrase]
-  (rate phrase negative_words))
+  (rate phrase data/negative-words))
 
 (defn positive-rating [phrase]
-  (rate phrase positive_words))
+  (rate phrase data/positive-words))
 
 (defn analyse [phrase]
   (let [negative (negative-rating phrase)
