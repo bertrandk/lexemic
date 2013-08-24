8aafec7bb4d313823e24b65cbddb1da5206919e6
bertrandk <b.karerangabo@gmail.com>
2013-08-24 16:14:46 -0400

    Add support for sentiment analysis from cmd line.

--- src/lexemic/core.cljs
+++ src/lexemic/core.cljs
@@ -1,5 +1,22 @@
-(ns lexemic.core)
+(ns lexemic.core
+  (:require [lexemic.sentiment.simple :as sentiment]))
 
-(defn -main [& command])
+(def ^:private usage-banner "Usage: lexemic [command] [target]")
+(def ^:private supported-commands #{"help" "sentiment"})
+
+(defn- show-help []
+  (do
+    (println usage-banner)))
+
+(defn- run [cmd text]
+  (condp = cmd
+    "sentiment" (println (sentiment/analyse text))))
+
+(defn -main [& args]
+  (let [command (first args)
+        input (second args)]
+    (if-let [cmd (supported-commands command)]
+      (run cmd input)
+      (show-help))))
 
 (set! *main-cli-fn* -main)
