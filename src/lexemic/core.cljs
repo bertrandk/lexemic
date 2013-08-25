84de90b863d9a0a5676d34ca751ee2103011f29b
bertrandk <b.karerangabo@gmail.com>
2013-08-25 16:10:20 -0400

    Add support for sentiment analysis from file.

--- src/lexemic/core.cljs
+++ src/lexemic/core.cljs
@@ -1,22 +1,28 @@
 (ns lexemic.core
-  (:require [lexemic.sentiment.simple :as sentiment]))
+  (:require [cljs.nodejs :as node]
+            [lexemic.sentiment.simple :as sentiment]))
+
+(def ^:private fs
+  (node/require "fs"))
 
 (def ^:private usage-banner "Usage: lexemic [command] [target]")
 (def ^:private supported-commands #{"help" "sentiment"})
 
-(defn- show-help []
-  (do
-    (println usage-banner)))
+(defn- help []
+  (str usage-banner))
 
 (defn- run [cmd text]
   (condp = cmd
-    "sentiment" (println (sentiment/analyse text))))
+    "sentiment" (sentiment/analyse text)))
 
 (defn -main [& args]
   (let [command (first args)
-        input (second args)]
+        target (second args)
+        input (if (.existsSync fs target)
+                (.readFileSync fs target "utf8")
+                target)]
     (if-let [cmd (supported-commands command)]
-      (run cmd input)
-      (show-help))))
+      (println (run cmd input))
+      (println (help)))))
 
 (set! *main-cli-fn* -main)
