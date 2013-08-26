5f3a941bfda22af55279cb652cc4508c29cc1c1e
bertrandk <b.karerangabo@gmail.com>
2013-08-26 11:28:51 -0400

    Make cmd line command handling more robust.

--- src/lexemic/core.cljs
+++ src/lexemic/core.cljs
@@ -2,23 +2,32 @@
   (:require [cljs.nodejs :as node]
             [lexemic.sentiment.simple :as sentiment]))
 
+(def ^:private version
+  (.-version (node/require "./package.json")))
 (def ^:private fs
   (node/require "fs"))
 
+(def ^:private spacer "")
+(def ^:private version (str "Lexemic v" version))
 (def ^:private usage-banner "Usage: lexemic [command] [implementation] [target...]")
 (def ^:private supported-commands #{"help" "sentiment"})
 
-(defn- help []
-  (str usage-banner))
+(defn- show-help []
+  (do
+    (println version)
+    (println spacer)
+    (println usage-banner)))
 
 (defn- run [cmd impl text]
   (condp = cmd
-    "sentiment" (sentiment/analyse text)))
+    "sentiment" (prn (sentiment/analyse text))
+    (show-help)))
 
 (defn -main [& args]
   (let [command (first args)
-        impl (when (.match (second args) #"^-{1,2}")
-               (second args))
+        impl (let [impl* (second args)]
+               (when (and impl* (.match impl* #"^-{1,2}"))
+                 impl*))
         targets (if impl
                   (drop 2 args)
                   (drop 1 args))
@@ -31,7 +40,7 @@
         contents (map get-string targets)
         input (print-str contents)]
     (if-let [cmd (supported-commands command)]
-      (println (run cmd impl input))
-      (println (help)))))
+      (run cmd impl input)
+      (show-help))))
 
 (set! *main-cli-fn* -main)
